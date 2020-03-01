f75ff80e3d3ccf4035a5ed9e118cf467fc4931ae
Mauricio Cleveland <mauricio.cleveland@gmail.com>
2020-03-01 17:55:03 -0300

    Added add/print methods for hash implementation

---
+++ hash/cli_hash.py
@@ -0,0 +1,36 @@
+from hash_core import hash_table
+
+# Helpers
+def is_numeric(order):
+    if not order.isnumeric():
+        print('A number is requested')
+        return False
+    return True
+
+def input_to_numeric(msg):
+    input_from_user = input(msg)
+    if is_numeric(input_from_user):
+        return int(input_from_user)
+    return -1
+
+def start():
+    hash_object = hash_table.HashTable()
+    while True:
+        order = input_to_numeric("options:\n\n1) Add node\n2) Delete node\n3) Search node\n4) Print hash\n5) Quit\n\nOption selected? ")
+        if order == 1:
+            node_value = input_to_numeric("Insert value: ")
+            hash_object.add_element(node_value)
+        if order == 2:
+            node_value = input_to_numeric("Delete value: ")
+            hash_object.remove_element(node_value)
+        if order == 3:
+            node_value = input_to_numeric("\nSearch value: ")
+            print('Found' if hash_object.find_element(node_value) is True else 'Not found')
+        
+        if order == 4:
+            hash_object.print_hash()
+
+        if order == 5:
+            break
+
+start()
--- hash/hash_core/hash_table.py
+++ hash/hash_core/hash_table.py
@@ -1,26 +1,55 @@
-# Bring packages to the current workspace.
+# Bring packages to the current workspace
 import sys, os
-sys.path.append(os.path.abspath(os.path.join('..', 'data-structures', 'linked-list')))
+sys.path.append(os.path.abspath(os.path.join('..', 'ds', 'linked_list')))
 
-from list_core import linked_list  # noqa
+from list_core import linked_list
 
 class HashTable:
     def __init__(self):
-        # Using a dictionary to mapping.
+        # Using a dictionary to mapping
         self.hash_table = {}
 
     def hash_function(self, value):
-        return len(value)
+        return len(str(value))
 
-    def check_colision(self, value):
-        if self.hash_function(value) in self.hash_table:
+    def check_colision(self, hash_of_value):
+        if hash_of_value in self.hash_table:
             return True
         return False
     
-    def add_element(self, index, value):
-        # check colision.
-        if self.check_colision(value):
-           return
+    def linked_list_generator(self, value, hash_of_value):
+        current_value = self.hash_table[hash_of_value]
+        if isinstance(current_value, linked_list.LinkedList):
+            # append value since the list was already created
+            current_value.add_node(value)
+            return current_value
+        
+        new_linked_list = linked_list.LinkedList()
+        new_linked_list.add_node(current_value)
+        new_linked_list.add_node(value)
+        return new_linked_list
+
+    def add_element(self, value):
+        hash_of_value = self.hash_function(value)
+        # check colision
+        if not self.check_colision(hash_of_value):
+           self.hash_table[hash_of_value] = value
+        # Collision detected, adding a linked list
+        else:
+            self.hash_table[hash_of_value] = self.linked_list_generator(value, hash_of_value)
+
+    def remove_element(self):
+        pass
+
+    def print_hash(self):
+        for key in self.hash_table:
+            if isinstance(self.hash_table[key], linked_list.LinkedList):
+                self.hash_table[key].print_list()
+            else:
+                print('Value: ', self.hash_table[key])
+
+    def find_element(self):
+        pass
         
         
         
