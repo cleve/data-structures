113702d75e747469616896dd1d8ff1db66823dd7
Mauricio Cleveland <mauricio.cleveland@gmail.com>
2020-03-24 17:06:09 -0300

    Added unittest for hash function

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -10,7 +10,21 @@ class TestHashMethods(unittest.TestCase):
             hash_object.add_element(element)
         self.assertEqual(elements, hash_object._count_elements())
 
-    
+    def test_hash_function(self):
+        hash_object = hash.hash_core.hash_table.HashTable()
+        # Len function""
+        hash_object.hash_selection = 1
+        string_1 = 'hello'
+        string_2 = 'hello_world'
+        string_3 = '#$%'
+        self.assertEqual(hash_object.hash_function(string_1), 5)
+        self.assertEqual(hash_object.hash_function(string_2), 11)
+        self.assertEqual(hash_object.hash_function(string_3), 3)
 
+        # ASCII function
+        hash_object.hash_selection = 2
+        self.assertEqual(hash_object.hash_function(string_1), '104101108108111')
+        self.assertEqual(hash_object.hash_function(string_2), '10410110810811195119111114108100')
+        
 if __name__ == '__main__':
     unittest.main()
