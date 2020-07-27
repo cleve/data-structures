0a4a7270dc2e9a84890d986df690b794a8a17253
Cleve <mauricio.cleveland@gmail.com>
2020-07-26 20:57:52 -0400

    Implementing priority queue

---
+++ priority_queue/priority_core/priority_queue.py
@@ -0,0 +1,13 @@
+class PriorityQueue:
+
+    def __init__(self):
+        self.array = []
+
+    def add_priority(self, priority):
+        pass
+
+    def pull_priority(self):
+        pass
+
+    def is_empty(self):
+        pass
